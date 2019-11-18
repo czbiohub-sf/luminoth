de96db9fe2747572abfa2502e251c03fa23e4ff9
pranathi.vemuri <pranathi93.vemuri@gmail.com>
2019-11-18 15:04:56 -0800

    simplifying split_train_val

--- luminoth/utils/split_train_val.py
+++ luminoth/utils/split_train_val.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import itertools
 import glob
 import math
 import natsort
@@ -70,6 +69,8 @@ def get_lumi_csv_df(bb_labels, images, output_image_format):
                             'ymax': int(row['y2']),
                             'label': label_name},
                            ignore_index=True)
+
+    # Cast required columns to integers
     if type(label_name) is str:
         cols = ['xmin', 'xmax', 'ymin', 'ymax']
         df[cols] = df[cols].applymap(np.int64)
@@ -77,6 +78,109 @@ def get_lumi_csv_df(bb_labels, images, output_image_format):
         cols = ['xmin', 'xmax', 'ymin', 'ymax', 'label']
         df[cols] = df[cols].applymap(np.int64)
     return df
+
+
+def add_basename_gather_df(filenames, input_image_format):
+    """
+    Writes to output_dir two folders train, val images and two csv files,
+    train.csv, val.csv
+
+    Args:
+        filenames: List of paths to comma separated txt file with
+            image_path,x1,y1,x2,y2,class_name
+        input_image_format: remove this format tag from the basename of the
+            image_path while adding base_path column
+
+    Returns:
+        bb_labels_df: Returns a dataframe with all the bounding boxes & labels
+            from all the paths in filenames. Also adds a column base_path which
+            is just the basename of a fullpath, e.g /data/bla/img.tif's
+            base_path would be img
+    """
+    dfs = []
+    for filename in filenames:
+        dfs.append(pd.read_csv(filenames))
+    bb_labels_df = pd.concate(dfs, ignore_index=True)
+    base_names = [
+        os.path.basename(row.image_path).replace(
+            input_image_format, "") for index, row in bb_labels_df.iterrows()]
+    bb_labels_df["base_path"] = pd.Series(base_names)
+    return bb_labels_df
+
+
+def get_image_paths_per_class(bb_labels_df):
+    """
+    Returns dict containing class name as the key and the
+    list of image paths containing the class annotation as values
+
+    Args:
+        bb_labels: Returns a dataframe with all the bounding boxes and labels
+            from all the paths in filenames. Also adds a column base_path which
+            is just the basename of a fullpath, e.g /data/bla/img.tif's
+            base_path would be img
+
+    Returns:
+        image_paths_per_class: dict containing class name as the key and the
+        list of image paths containing the class annotation as values
+    """
+    # Print meta for each unique class
+    class_labels = np.unique(bb_labels_df.class_name)
+    image_paths_per_class = {}
+    for class_name in class_labels:
+        # This dict collects all the unique images of a class
+        filtered_df = bb_labels_df[bb_labels_df['class_name'] == class_name]
+        images = np.unique(filtered_df['image_path']).tolist()
+        image_paths_per_class[class_name] = images
+        print('There are {} images with  {} {} classes in the dataset'.format(
+            len(image_paths_per_class[class_name]),
+            len(filtered_df),
+            class_name))
+    return image_paths_per_class
+
+
+def write_lumi_images_csv(
+        images,
+        path,
+        input_image_format,
+        output_image_format,
+        bb_labels,
+        output_csv_path):
+    """
+    Copies images to path with a output_image_format
+    and writes a csv containing LUMI_CSV_COLUMNS
+    listing out the images and annotations for the same images
+
+    Args:
+        images: list of images to copy to path
+        path: Directory to output the scaled uint8 jpg files or files
+            of output_image_format to
+        input_image_format: raw input image data format
+        output_image_format: output image format,
+            lumi currently accepts only jpg for inputs, so the train and
+            val folders created would be saving input images as uint8 jpgs to
+            the output_dir. In that case the output_image_format would be .jpg
+        bb_labels: Returns a dataframe with all the bounding boxes and labels
+            from all the paths in filenames. Also adds a column base_path which
+            is just the basename of a fullpath, e.g /data/bla/img.tif's
+            base_path would be img
+
+    Returns:
+        Writes images to path and writes a csv file containing LUMI_CSV_COLUMNS
+        to output_csv_path
+    """
+    # Save each classes' images to given path as output_image_format
+    os.makedirs(path, exist_ok=True)
+    for index, original_path in enumerate(images):
+        new_path = os.path.join(path, os.path.basename(original_path))
+        new_path = new_path.replace(input_image_format, output_image_format)
+        image = io.imread(original_path)
+        image = (image / image.max() * 255).astype(np.uint8)
+        io.imsave(new_path, io.imread(original_path))
+    images = natsort.natsorted(
+        glob.glob(os.path.join(path, "*" + output_image_format)))
+    print('number of images in path : ', len(images))
+    df = get_lumi_csv_df(bb_labels, images, output_image_format)
+    df.to_csv(output_csv_path)
 
 
 def split_data_to_train_val(
@@ -89,122 +193,74 @@ def split_data_to_train_val(
         output_dir,
         output_image_format):
     """
-    Writes to output_dir two folders train, val and two csv files,
+    Writes to output_dir two folders train, val images and two csv files,
     train.csv, val.csv
 
     Args:
-        filenames: Full path to comma separated txt file with
+        filenames: List of paths to comma separated txt file with
             image_path,x1,y1,x2,y2,class_name
         class_labels: list of classes
         output_dir: Full path to save the train,
             val folders and their csv files
         random_seed: Randomize the images so no
-            two continuos slices go to the train or validation directory
+            two continuous slices go to the train or validation directory
         percentage: Percentage of data for training
-            and 1- percentage images are copied to validation directory
+            and 1 - percentage images are copied to validation directory
         filter_dense_anns: It is assumed that an image
             consists of one densely annotated class and sparsely
             annotated other class labels.
-            So, images with just dense classes are filtered out
-        input_image_format: raw image data format
-        output_dir: Directory to output the scaled uint8 jpg files, data
+            So, images with just dense classes are filtered out if this flag
+            is set to True
+        input_image_format: raw input image data format
+        output_dir: Directory to output the scaled uint8 jpg files or files
+            of output_image_format to
         output_image_format: lumi output image format,
-            lumi currently accepts only jpg for inputs
+            lumi currently accepts only jpg for inputs, so the train and
+            val folders created would be saving input images as uint8 jpgs to
+            the output_dir
     """
     random.seed(random_seed)
-    # Add base_path column to filter into training and validation images later
-    dfs = []
-    for filename in filenames:
-        dfs.append(pd.read_csv(filenames))
-    bb_labels = pd.concate(dfs, ignore_index=True)
-    base_names = [
-        os.path.basename(row.image_path).replace(
-            input_image_format, "") for index, row in bb_labels.iterrows()]
-    bb_labels["base_path"] = pd.Series(base_names)
+    bb_labels = add_basename_gather_df(filenames, input_image_format)
 
-    # Print meta for each class in class_labels
-    image_ids_classes = {}
-    for class_name in class_labels:
+    # Get unique images per classes
+    image_paths_per_class = get_image_paths_per_class(bb_labels, class_labels)
 
-        # This dict collects all the unique images of a class
-        filtered_df = bb_labels[bb_labels['class_name'] == class_name]
-        print('There are {} {} classes in the dataset'.format(
-            len(filtered_df), class_name))
-        image_ids_classes[class_name] = np.unique(filtered_df['image_path'])
-        print('There are {} images with {} classes in the dataset'.format(
-            len(image_ids_classes[class_name]), class_name))
-    # Balancing class count by
-    # taking into account only images with classes that are sparsely present
-    # these images are
+    # Balancing class count by taking into account only images with
+    # classes that are sparsely present these images are
     # expected to contain the other class that has lots of annotations
     if filter_dense_anns:
         max_class_count = max(
-            [len(value) for key, value in image_ids_classes.items()])
+            [len(value) for key, value in image_paths_per_class.items()])
         max_class_count_name = [
-            key for key, value in image_ids_classes.items() if len(
+            key for key, value in image_paths_per_class.items() if len(
                 value) == max_class_count][0]
-        image_ids_classes.pop(max_class_count_name)
+        image_paths_per_class.pop(max_class_count_name)
 
-    # Get unique images that contains desired classes
-    all_imgs = np.unique(list(itertools.chain.from_iterable(
-        [value for key, value in image_ids_classes.items()]))).tolist()
+    all_imgs = np.unique(bb_labels['image_path']).tolist()
     all_imgs_length = len(all_imgs)
-    all_imgs = natsort.natsorted(all_imgs)
     random.shuffle(all_imgs)
 
-    # Prepare dataset format for faster rcnn code
-    # (fname_path, xmin, xmax, ymin, ymax, class_name)
-    # train: 0.9
-    # validation: 0.1
-
-    # Save images to train and val directory
-    train_path = os.path.join(output_dir, 'train')
-    os.makedirs(train_path, exist_ok=True)
-    val_path = os.path.join(output_dir, 'val')
-    os.makedirs(val_path, exist_ok=True)
     training_image_index = math.floor(percentage * all_imgs_length)
-    train_imgs = np.unique(all_imgs[:training_image_index]).tolist()
-    val_imgs = np.unique(all_imgs[training_image_index:]).tolist()
-    # Save each classes' images to train directory as jpgs
 
-    for index, original_path in enumerate(train_imgs):
-        print(index)
-        new_path = os.path.join(train_path, os.path.basename(original_path))
-        new_path = new_path.replace(input_image_format, output_image_format)
-        image = io.imread(original_path)
-        image = (image / image.max() * 255).astype(np.uint8)
-        io.imsave(new_path, io.imread(original_path))
+    write_lumi_images_csv(
+        all_imgs[:training_image_index],
+        os.path.join(output_dir, "train"),
+        input_image_format,
+        output_image_format,
+        bb_labels,
+        os.path.join(os.path.dirname(output_dir), 'train.csv'))
 
-    # Save each classes' images to val directory as jpgs
-    for index, original_path in enumerate(val_imgs):
-        print(index)
-        new_path = os.path.join(val_path, os.path.basename(original_path))
-        new_path = new_path.replace(input_image_format, output_image_format)
-        image = io.imread(original_path)
-        image = (image / image.max() * 255).astype(np.uint8)
-        io.imsave(new_path, io.imread(original_path))
-
-    train_images = natsort.natsorted(
-        glob.glob(os.path.join(train_path, "*" + output_image_format)))
-    val_images = natsort.natsorted(
-        glob.glob(os.path.join(val_path, "*" + output_image_format)))
-    print('number of training images: ', len(train_images))
-    print('number of validation images: ', len(val_images))
-
-    # create metadata dataframes for training and validation images
-    train_df = get_lumi_csv_df(bb_labels, train_images, output_image_format)
-    val_df = get_lumi_csv_df(bb_labels, val_images, output_image_format)
-
-    # Save the csvs
-    train_df.to_csv(os.path.join(output_dir, 'train.csv'))
-    val_df.to_csv(os.path.join(output_dir, 'val.csv'))
-
-
+    write_lumi_images_csv(
+        all_imgs[training_image_index:],
+        os.path.join(output_dir, "val"),
+        input_image_format,
+        output_image_format,
+        bb_labels,
+        os.path.join(os.path.dirname(output_dir), 'val.csv'))
 
 
 @click.command(help="Split and arrange images into 2 folders for grayscale jpgs for train, and validation, save bounding boxes and labels for the corresponding images in train.csv and val.csv")  # noqa
 @click.argument("filenames", nargs=-1) # noqa
-@click.option("--class_labels", help="List of labels for classes", multiple=True, required=True) # noqa
 @click.option("--percentage", help="Percentage of images to split into training folder, rest of the images are equally divided to validation", required=False, type=float, default=0.9) # noqa
 @click.option("--random_seed", help="Random seed to split data into training, validation images", required=False, type=int, default=43) # noqa
 @click.option('--filter_dense_anns', help="Filter out images with only the dense class annotations", required=False)  # noqa
@@ -212,7 +268,6 @@ def split_data_to_train_val(
 @click.option('--output_dir', help="Absolute path to folder containing train, validation scaled uint8 jpg images and their annotations in csv file", required=True, type=str)  # noqa
 def split_train_val(
         filenames,
-        class_labels,
         percentage,
         random_seed,
         filter_dense_anns,
@@ -222,7 +277,6 @@ def split_train_val(
     assert percentage < 1.0
     split_data_to_train_val(
         filenames,
-        class_labels,
         percentage,
         random_seed,
         filter_dense_anns,
