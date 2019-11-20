1ef3f8e6a88f6ea4cb88dceb5f60a22f6990d427
pranathi.vemuri <pranathi93.vemuri@gmail.com>
2019-11-20 12:52:44 -0800

    more arguments

--- luminoth/utils/data_aug_demo.py
+++ luminoth/utils/data_aug_demo.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 
 import click
@@ -36,16 +37,21 @@ def get_data_aug_images(image, bboxes):
     return augmented_images
 
 
-def mosaic_data_aug(input_png, csv_path, tile_size, fill_value, output_png):
+def mosaic_data_aug(
+        input_image, input_image_format,
+        csv_path, image_path_column,
+        tile_size, fill_value, output_png):
     """
     Mosaic data augmented images after resizing them to tile_size.
     all the pixels that don't fit in the stitched image shape
     by the resized tiles are filled with fill_value
 
     Args:
-        input_png: str Input png to augment
+        input_image: str Input png to augment
+        input_image_format: str Format of the input images
         csv_path: csv containing image_id,xmin,xmax,ymin,ymax,label
             for the input_png. Bounding boxes in the csv are augmented
+        image_path_column: str name of the image_path_column
         tile_size: tuple that each image in the mosaic is resized to
         fill_value: fill the tiles that couldn't be filled with the images
         output_png: write the stitched mosaic image to
@@ -56,10 +62,15 @@ def mosaic_data_aug(input_png, csv_path, tile_size, fill_value, output_png):
         tile_size[0] * sqrt(len(images_in_path)) * tile_size[1] * sqrt(
         len(images_in_path)).
     """
-    image = cv2.imread(input_png, cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread(input_image, cv2.IMREAD_GRAYSCALE)
     df = pd.read_csv(csv_path)
+    basename = os.path.basename(input_image).replace(input_image_format, "")
+    tmp_df = df[
+        [True if os.path.basename(row[image_path_column]).replace(
+            input_image_format, "").endswith(
+            basename) else False for index, row in df.iterrows()]]
     bboxes = []
-    for index, row in df.iterrows():
+    for index, row in tmp_df.iterrows():
         bboxes.append([
             row['xmin'],
             row['xmax'],
@@ -76,13 +87,20 @@ def mosaic_data_aug(input_png, csv_path, tile_size, fill_value, output_png):
 
 
 @click.command(help="Save one assembled mosaic filled with data augmented images for png")  # noqa
-@click.option("--input_png", help="Input png to augment", required=True, type=str) # noqa
+@click.option("--input_image", help="Input data to augment", required=True, type=str) # noqa
+@click.option("--input_image_format", help="Input image format", required=True, type=str) # noqa
 @click.option("--csv_path", help="Csv containing image_id,xmin,xmax,ymin,ymax,label.Bounding boxes in the input png to augment", required=True, type=str) # noqa
+@click.option("--image_path_column", help="Name of the image path column, it is often image_id for lumi output, or could be image_path if saved outside of lumi", required=True, type=str) # noqa
 @click.option("--tile_size", help="[x,y] list of tile size in x, y", required=False, multiple=True) # noqa
 @click.option("--fill_value", help="fill the image with zeros or the first intensity at [0,0] in the image", required=False, type=str) # noqa
 @click.option("--output_png", help="Absolute path to folder name to save the data aug mosaiced images to", required=True, type=str) # noqa
-def data_aug_demo(input_png, tile_size, fill_value, output_png):
-    mosaic_data_aug(input_png, tile_size, fill_value, output_png)
+def data_aug_demo(
+        input_image, input_image_format, csv_path,
+        image_path_column, tile_size, fill_value, output_png):
+    mosaic_data_aug(
+        input_image, input_image_format,
+        csv_path, image_path_column,
+        tile_size, fill_value, output_png)
 
 
 if __name__ == '__main__':
