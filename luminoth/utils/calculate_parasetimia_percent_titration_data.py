8408de73fe4b9d54abffe70930affad5a31a1b20
pranathi.vemuri <pranathi93.vemuri@gmail.com>
2020-06-13 20:18:11 -0700

    add parasetimia percentage utils program

---
+++ luminoth/utils/calculate_parasetimia_percent_titration_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import sys
+
+
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    df = pd.read_csv(input_file)
+    total_classes = len(df)
+    parasite_classes = ["ring", "schizont", "troph"]
+    filtered_df = df[df['label'].isin(parasite_classes)]
+    parasitemia_percentage = [len(filtered_df) / len(df)] + 100
+    print(parasitemia_percentage)
