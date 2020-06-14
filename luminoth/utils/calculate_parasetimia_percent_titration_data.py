35b3e8fceffc328de23f1ed570e71cde5222675a
Pranathi Vemuri <pranathi93.vemuri@gmail.com>
2020-06-14 12:09:23 -0700

    titration point string

--- luminoth/utils/calculate_parasetimia_percent_titration_data.py
+++ luminoth/utils/calculate_parasetimia_percent_titration_data.py
@@ -14,7 +14,11 @@ if __name__ == "__main__":
         image_path = row["image_id"]
         break
     print(image_path)
-    titration_point = int(
-        os.path.basename(image_path).split("Titration_point")[-1].split("_")[0])
+    if "Titration_point" in image_path:
+        titration_point = int(
+            os.path.basename(image_path).split("Titration_point")[-1].split("_")[0]
+    elif "Titration_Point" in image_path:
+       titration_point = int(
+           os.path.basename(image_path).split("Titration_Point")[-1].split("_")[0])
     parasitemia_percentage = (len(filtered_df) / len(df)) * 100
     print(titration_point, parasitemia_percentage)
